e153f4258ac09dbe4546aa1fcaa17f9c024d2a04
Josh Date <date0010@me.com>
2014-05-13 15:18:43 -0400

    initial commit for jquery methods. adding example one in progress and general page skelton

---
+++ yeomanOne/app/scripts/main.js
@@ -0,0 +1,32 @@
+// 15 different jquery methods
+// using .click() to trigger each method for a demo
+// 1 - .hover() & .animate()
+
+
+// var swipeStyles = {"right": "88px","z-index": "5"};
+
+// $('.swipe-box:before').hover(function() {
+// 	$(this).stop().animate(.css(swipeStyles){duration:500})
+// })
+
+// $('.swipe-box:before').hover(function() {
+//   	$('.swipe-box:before').animate({
+// 	    'right': '88px',
+// 	    'z-index': '5';
+//   	}, 5000,);
+// });
+	
+// 2 - 
+// 3 - 
+// 4 - 
+// 5 - 
+// 6 - 
+// 7 - 
+// 8 - 
+// 9 - 
+// 10 - 
+// 11 - 
+// 12 - 
+// 13 - 
+// 14 - 
+// 15 - 
