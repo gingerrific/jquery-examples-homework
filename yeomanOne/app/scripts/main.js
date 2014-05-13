a9d9d726016fbed5c1b9e917af8f98a071625038
Josh Date <date0010@me.com>
2014-05-13 19:33:33 -0400

    added second and third examples using clone and hide

--- yeomanOne/app/scripts/main.js
+++ yeomanOne/app/scripts/main.js
@@ -2,15 +2,37 @@
 
 // 15 different jquery methods
 // using .click() to trigger each method for a demo
-// 1 - .addClass()
 
-$('.button-one').click(function (){
-	$('.example-one').find('li').toggleClass('swipe-box');
+//// Example One //////////////////////////////////
+//// .hover() & .animate() ////////////////////////
+
+$('.toggle-class-button').click(function (){
+	$('.toggle-class-example').find('li').toggleClass('swipe-box'); // adds new class that also contain pseudo elements
 });
 
-	
-// 2 - 
-// 3 - 
+//// Example Two //////////////////////////////////
+//// .hide()  /////////////////////////////////////
+
+
+$('.hide-example-button').click(function () { // initiates demo
+	$('.circle').show('slow'); //resets circles when clicking "demo button"
+	$('.circle').click(function(){
+		$(this).hide('slow'); //hides clicked circle
+	});
+});
+
+//// Example Three ////////////////////////////////
+//// .clone() /////////////////////////////////////
+
+$('.clone-example-button').click(function () {
+	$('.purple-button').click(function () {
+		$('.box-one').first().clone().prependTo('.clone-example');
+	});
+	$('.pink-button').click(function () {
+		$('.box-two').first().clone().prependTo('.clone-example');
+	});
+});
+
 // 4 - 
 // 5 - 
 // 6 - 
