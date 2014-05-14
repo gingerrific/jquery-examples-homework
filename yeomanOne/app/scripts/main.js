31d9b68cb92580286929295a35d74ef0e72bcab1
Josh Date <date0010@me.com>
2014-05-13 22:32:59 -0400

    added through example 6. added on animate and mouseevents

--- yeomanOne/app/scripts/main.js
+++ yeomanOne/app/scripts/main.js
@@ -24,7 +24,20 @@ $('.hide-example-button').click(function () { // initiates demo
 //// Example Three ////////////////////////////////
 //// .clone() /////////////////////////////////////
 
+var cloneExampleClone = $('.clone-example').clone();
+
 $('.clone-example-button').click(function () {
+	$('.purple-button').click(function () { //clicking the purple button prepends a clone of the purple box
+		$('.box-one').first().clone().prependTo('.clone-example');
+	});
+	$('.pink-button').click(function () { //clicking the pink button prepends a clone of the pink box
+		$('.box-two').first().clone().prependTo('.clone-example');
+	});
+});
+
+// on double click a clone of the initial state is restored and the initial click state is passed to avoid having to single click to start again.
+$('.clone-example-button').dblclick(function () {
+	$('.clone-example').replaceWith(cloneExampleClone);
 	$('.purple-button').click(function () {
 		$('.box-one').first().clone().prependTo('.clone-example');
 	});
@@ -33,10 +46,53 @@ $('.clone-example-button').click(function () {
 	});
 });
 
-// 4 - 
-// 5 - 
-// 6 - 
-// 7 - 
+//// Example Four /////////////////////////////////
+//// .on() ////////////////////////////////////////
+
+$('.on-example-button').click(function (){
+	$('.on-inner-box').on('mouseenter', function () {
+		$(this).css('border','4px solid #5FF756');
+	});
+	$('.on-inner-box').on('mouseleave', function () {
+		$(this).css('border','4px solid #FF7947');
+	});
+});
+
+
+
+//// Example Five /////////////////////////////////
+//// .animate() ///////////////////////////////////
+
+$('.animation-example-button').click(function (){
+	$('.up-button').click(function() {
+		$( ".animated-box" ).animate({ 'top': '-=20px' }, "fast" );
+	});
+	$('.down-button').click(function() {
+		$( ".animated-box" ).animate({ 'top': '+=20px' }, "fast" );
+	});
+	$('.right-button').click(function() {
+		$( ".animated-box" ).animate({ 'left': '+=20px' }, "fast" );
+	});
+	$('.left-button').click(function() {
+		$( ".animated-box" ).animate({ 'left': '-=20px' }, "fast" );
+	});
+});
+
+
+//// Example Six //////////////////////////////////
+//// mousemove .text() event.pageX/Y //////////////
+
+$('.mouse-location-button').click(function (){
+	$('.example-box').on( 'mousemove', function( event ) {
+		$('.mouse-reporter-one').text( "Offset from the left of the page: " + event.pageX);
+		$('.mouse-reporter-two').text( "Offset from the top of the page: " + event.pageY + '  (including scroll)');
+	});
+});
+
+//// Example Seven ////////////////////////////////
+//// .animate() ///////////////////////////////////
+
+
 // 8 - 
 // 9 - 
 // 10 - 
